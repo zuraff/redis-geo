7bab6651d6031d04f7344ec108381f3b64e84605
zuraff <pawel.kedzior@gmail.com>
2017-03-23 22:24:10 -0500

    Configuring my server

--- deploy-envs.sh
+++ deploy-envs.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 # set environment variables used in deploy.sh and AWS task-definition.json:
-export IMAGE_NAME=netcoreapps-redisgeo
+export IMAGE_NAME=AtoLogic.WellDone
 export IMAGE_VERSION=latest
 
-export AWS_DEFAULT_REGION=us-east-1
+export AWS_DEFAULT_REGION=eu-central-1
 export AWS_ECS_CLUSTER_NAME=default
-export AWS_VIRTUAL_HOST=redisgeo.netcore.io
+export AWS_VIRTUAL_HOST=docker.listazgrzewow.pl
 
 # set any sensitive information in travis-ci encrypted project settings:
 # required: AWS_ACCOUNT_NUMBER, AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY
